db1b29e8f7b2c2148ec1bdf1eb2aeb6774668177
Jason Rigby <jason.rigby@monash.edu>
2016-09-21 15:04:45 +1000

    add some run scripts for testing/iex/dev

---
+++ local_iex.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export MIX_ENV=dev
+export DATABASE_URL=ecto://postgres:postgres@localhost/edm_backend_dev
+export EDM_OAUTH_CLIENT_ID=oidcCLIENT
+export EDM_OAUTH_CLIENT_SECRET=91c0fabd17a9db3cfe53f28a10728e39b7724e234ecd78dba1fb05b909fb4ed98c476afc50a634d52808ad3cb2ea744bc8c3b45b7149ec459b5c416a6e8db242
+export EDM_OAUTH_DISCOVERY_URL=http://localhost:5000/.well-known/openid-configuration
+iex -S mix
