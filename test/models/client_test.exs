87e1d323240868167637206c3e2a2812f1367f6e
Jason Rigby <jason.rigby@monash.edu>
2016-09-21 15:18:36 +1000

    changed client model so only uuid is required

--- test/models/client_test.exs
+++ test/models/client_test.exs
@@ -25,19 +25,10 @@ defmodule EdmBackend.ClientModelTest do
 
   test "invalid client values" do
     client1 = %Client{} |> Client.changeset(%{
-      uuid: "acfd51b2-0972-426a-acf1-2827f77fc97d"
-    })
-    client2 = %Client{} |> Client.changeset(%{
       ip_address: "127.0.0.1"
     })
-    client3 = %Client{} |> Client.changeset(%{
-      uuid: "acfd51b2-0972-426a-acf1-2827f77fc97q",
-      ip_address: "127.0.0.1"
-    })
-    
+
     refute client1.valid?
-    refute client2.valid?
-    refute client3.valid?
   end
 
   test "client uniqueness" do
