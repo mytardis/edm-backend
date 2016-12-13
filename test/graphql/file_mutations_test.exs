a5226254295d201e7652c07424ad4968a57ccbf3
Grischa Meyer <grischa@gmail.com>
2016-12-13 13:16:09 +1100

    updated tests

--- test/graphql/file_mutations_test.exs
+++ test/graphql/file_mutations_test.exs
@@ -19,12 +19,28 @@ defmodule EdmBackend.FileMutationTest do
 
   test "creating a file", context do
     client = context[:client]
+    file_info = %{
+        "filepath" => "testfile3.txt",
+        "atime" => "2013-06-05T06:40:25.000Z",
+        "birthtime" => "2013-06-05T06:40:25.000Z",
+        "ctime" => "2013-06-05T06:40:25.000Z",
+        "mode" => 100644,
+        "mtime" => "2013-06-05T06:40:25.000Z",
+        "size" => 12
+       }
+    file_info_json = Poison.encode!(file_info)
     query = """
 {"query": "mutation getOrCreateFile($input: GetOrCreateFileInput!) {
   getOrCreateFile(input: $input) {
             clientMutationId
             file {
-            	filepath
+              filepath
+              size
+              atime
+              ctime
+              mtime
+              birthtime
+              mode
             }
         }
     }",
@@ -32,15 +48,7 @@ defmodule EdmBackend.FileMutationTest do
   "input": {
     "clientMutationId": "123",
     "source": {"name": "test source"},
-    "file": {
-      "filepath": "testfile3.txt",
-        "size": 12,
-        "mtime": "2013-06-05T06:40:25.000Z",
-      "atime": "2013-06-05T06:40:25.000Z",
-      "ctime": "2013-06-05T06:40:25.000Z",
-      "birthtime": "2013-06-05T06:40:25.000Z",
-      "mode": 100644
-    }
+    "file": #{file_info_json}
   }
 }
 }
@@ -49,6 +57,7 @@ defmodule EdmBackend.FileMutationTest do
       message: "Field `getOrCreateFile': Not logged in"}])
     assert_data(query, %{"getOrCreateFile" => %{
       "clientMutationId" => "123",
-      "file" => %{"filepath" => "testfile3.txt"}}}, client)
+      "file" => file_info,
+       }}, client)
   end
 end
